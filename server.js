c9bd20f358f9c3f0f80e59b7314a5c90371b3bb6
Matt Keane <mattpkeane@gmail.com>
2021-07-27 15:23:12 -0500

    added static directory

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 	res.render('home.ejs');
 });
 
-app.listen(PORT, err => {
+app.listen(PORT, () => {
 	const d = new Date();
 	console.log(`${d.toLocaleString()}: listening on Port ${PORT}.`)
 });
